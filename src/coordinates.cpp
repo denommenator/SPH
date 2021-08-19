1d37df8adb706c7dee6a9623ce717f55c554a819
Robert Denomme <denommenator@gmail.com>
2021-08-18 22:28:52 -0400

    Moved the State class over into the coordinates.hpp translation unit.

--- src/coordinates.cpp
+++ src/coordinates.cpp
@@ -148,4 +148,22 @@ std::ostream& operator<<(std::ostream &strm, const Coordinates &qs)
 	return strm;
 }
 
+
+State::State(Coordinates qs, 
+		Coordinates q_dots)
+	: qs{qs}, q_dots{q_dots}, coordinate_ids{qs.coordinate_ids}
+	{}
+
+State::State(CoordinateIDManager coordinate_ids)
+	: qs{coordinate_ids}, q_dots{coordinate_ids}, coordinate_ids{coordinate_ids}
+	{}
+
+
+std::ostream& operator<<(std::ostream &strm, const State &s)
+{
+	strm << "qs: " << s.qs << std::endl;
+	strm << "q_dots" << s.q_dots << std::endl;
+	return strm;
+}
+
 } //namespace SPH
--- test/coordinates.test.cpp
+++ test/coordinates.test.cpp
@@ -55,7 +55,66 @@ SCENARIO("Coordinates can be initialized from a CoordinateIDManager", "[coordina
 };
 
 
+SCENARIO("we can initialize a state vector with coordinate ids and define qs and q_dots later", "[state]")
+{
+	SECTION("We can initialize a state vector from qs and q_dots")
+	{
+		CoordinateIDManager ids= {"01", "02", "03"};
 
+		State initial_state{ids};
+		Coordinates q_not{ids};
+
+		initial_state.qs["01"] << 0,1;
+		initial_state.qs["02"] << 2,3;
+		initial_state.qs["03"] << 4,5;
+
+		initial_state.q_dots["01"] << 6,7;
+		initial_state.q_dots["02"] << 8,9;
+		initial_state.q_dots["03"] << 10,11;
+
+		q_not["01"] << 0,0; //not the same
+		q_not["02"] << 2,3;
+		q_not["03"] << 4,5;
+
+		
+
+		REQUIRE(initial_state.qs == initial_state.qs);
+		REQUIRE(initial_state.q_dots == initial_state.q_dots);
+		REQUIRE_FALSE(initial_state.qs == q_not);
+
+	}
+}
+
+
+SCENARIO("we can initialize a state vector with qs and q_dots", "[state]")
+{
+	SECTION("We can initialize a state vector from qs and q_dots")
+	{
+		CoordinateIDManager ids= {"01", "02", "03"};
+		Coordinates qs{ids}, q_dots{ids}, q_not{{"01", "02", "03"}};
+
+		qs["01"] << 0,1;
+		qs["02"] << 2,3;
+		qs["03"] << 4,5;
+
+		q_dots["01"] << 6,7;
+		q_dots["02"] << 8,9;
+		q_dots["03"] << 10,11;
+
+		q_not["01"] << 0,0; //not the same
+		q_not["02"] << 2,3;
+		q_not["03"] << 4,5;
+
+		State initial_state{qs, q_dots};
+
+		REQUIRE(initial_state.qs == qs);
+		REQUIRE(initial_state.q_dots == q_dots);
+		REQUIRE_FALSE(initial_state.qs == q_not);
+
+		
+
+	}
+}
 
 
 
